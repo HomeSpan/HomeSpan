d3029af569489b21559ce40a46c90a49a31cd7b1
Gregg <gregg.berman@me.com>
2020-10-05 21:33:55 -0500

    Delete extras.ino

--- src/extras/extras.ino
+++
@@ -1,5 +0,0 @@
-
-#error THIS IS NOT COMPILABLE CODE
-
-This is a dummy .ino file that allows you to easily edit the contents of this library using the Arduino IDE.
-The code is NOT designed to be compiled from this point.  Compile and test the library using one of the examples.
