2eb70f1d1bad950c8e147bd74cdba4076a952316
Gregg <homespan@icloud.com>
2023-06-09 23:15:38 -0500

    Changed links from HomeStep to HomeSpan

--- src/src/extras/StepperControl.h
+++ src/src/extras/StepperControl.h
@@ -3,7 +3,7 @@
  *  
  *  Copyright (c) 2023 Gregg E. Berman
  *  
- *  https://github.com/HomeSpan/HomeStep
+ *  https://github.com/HomeSpan/HomeSpan
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
