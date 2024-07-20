ebf277bbc35f68f02ec712307de7edcf20663c3c
Gregg <homespan@icloud.com>
2024-07-20 14:49:46 -0500

    RMT Testing

--- src/src/extras/extras.ino
+++ src/src/extras/extras.ino
@@ -25,26 +25,72 @@
  *  
  ********************************************************************************/
  
-#include "PwmPin.h"
+//#include "PwmPin.h"
+//
+//LedPin led(13);
+//
+//void setup() {
+// 
+//  Serial.begin(115200);
+//  delay(1000);
+//
+//  Serial.print("\n\nReady\n\n");
+//  
+//}
+//
+////////////////////////////////////////
+//
+//void loop(){
+//
+//  led.fade(100,1000);
+//  while(led.fadeStatus()==LedPin::FADING);
+//  led.fade(0,1000);
+//  while(led.fadeStatus()==LedPin::FADING);
+//
+//}
 
-LedPin led(13);
+#include "RFControl.h"
 
-void setup() {
+void setup() {     
  
-  Serial.begin(115200);
-  delay(1000);
+  Serial.begin(115200);           // start the Serial interface
+  Serial.flush();
+  delay(1000);                    // wait for interface to flush
 
-  Serial.print("\n\nReady\n\n");
+  Serial.print("\n\nHomeSpan RF Transmitter Example\n\n");
+
+  RFControl rf(13);               // create an instance of RFControl with signal output to pin 13 of the ESP32
+
+  rf.clear();                     // clear the pulse train memory buffer
+
+  rf.add(5000,5000);              // create a pulse train with three 5000-tick high/low pulses
+  rf.add(5000,5000);
+  rf.add(5000,10000);             // double duration of final low period
+
+  Serial.print("Starting 4 cycles of three 500 ms on pulses...");
   
-}
+  rf.start(4,100);                // start transmission of 4 cycles of the pulse train with 1 tick=100 microseconds
 
-//////////////////////////////////////
+  Serial.print("Done!\n");
+
+  delay(2000);
+
+  rf.clear();
+
+  for(int i=1000;i<10000;i+=1000)
+    rf.add(i,10000-i);
+  rf.add(10000,10000);
+  
+  Serial.print("Starting 3 cycles of 100-1000 ms pulses...");
+  
+  rf.start(3,100);                // start transmission of 3 cycles of the pulse train with 1 tick=100 microseconds
+
+  Serial.print("Done!\n");
+  
+  Serial.print("\nEnd Example");
+  
+} // end of setup()
 
 void loop(){
 
-  led.fade(100,1000);
-  while(led.fadeStatus()==LedPin::FADING);
-  led.fade(0,1000);
-  while(led.fadeStatus()==LedPin::FADING);
-
-}
+} // end of loop()
