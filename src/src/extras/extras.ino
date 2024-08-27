31e39a7b5a57b853ca6ab7e1d9c89377b7ab6ea5
Gregg <homespan@icloud.com>
2024-08-27 18:02:00 -0500

    Prepare to test other ESP32 chips

--- src/src/extras/extras.ino
+++ src/src/extras/extras.ino
@@ -28,6 +28,14 @@
 #include "Pixel.h"
 #include "RFControl.h"
 
+#define PIXEL_PIN   23
+#define LED_PIN     13
+
+#define NCYCLES     4
+#define COUNT       5
+#define ONTIME      5000
+#define OFFTIME     5000
+
 void setup(){     
  
   Serial.begin(115200);           // start the Serial interface
@@ -35,7 +43,8 @@ void setup(){
 
   Serial.print("\n\nHomeSpan Pixel+RF Example\n\n");
 
-  Pixel px(23);
+  Pixel px(PIXEL_PIN);
+  RFControl rf(LED_PIN);
   
   Pixel::Color c[8]={
     Pixel::RGB(255,0,0),
@@ -59,119 +68,43 @@ void setup(){
     Pixel::HSV(240,100,10)
   };
 
-for(int i=0;i<4;i++){
-    px.set(c,8);
-    delay(1000);        
-    px.set(d,8);
-    delay(1000);
-}
+  Serial.printf("Starting cycles of RGB pattern...\n");
 
-  long int x,y;
-
-  RFControl rfi(21,false);
-  RFControl rf(13);              // use REF TICK
-
-  rf.clear();                     // clear the pulse train memory buffer
-  rfi.clear();
-
-  #define COUNT   5
-  #define ONTIME  5000
-  #define OFFTIME 5000  
-
-  for(int i=0;i<COUNT;i++)
-    rfi.add(ONTIME*80,OFFTIME*80);
-  rfi.phase(OFFTIME*80,LOW);
+  for(int i=0;i<NCYCLES;i++){
+      px.set(c,8);
+      delay(1000);        
+      px.set(d,8);
+      delay(1000);
+  }
   
+  rf.clear();                    // clear the pulse train memory buffer
+
   for(int i=0;i<COUNT;i++)
     rf.add(ONTIME,OFFTIME);
   rf.phase(OFFTIME,LOW);
 
-//  rf.enableCarrier(10,0.4);
-
-  Serial.print("Starting cycles of pulses...\n");
+  long int x,y;
+  
+  Serial.printf("Starting cycles of pulses without carrier...\n");
   x=millis();
-  rfi.start(4,100);                // start transmission of 4 cycles of the pulse train with 1 tick=100/80 microseconds
+  rf.start(NCYCLES,100);
   y=millis();
-  Serial.println(y-x);
+  Serial.printf("Total time: %ld ms\n",y-x);
 
-  Serial.print("Starting cycles of pulses...\n");
+  rf.enableCarrier(10,0.4);
+
+  Serial.printf("Starting cycles of pulses with 10 Hz carrier...\n");
   x=millis();
-  rf.start(4,100);                // start transmission of 4 cycles of the pulse train with 1 tick=100/80 microseconds
+  rf.start(NCYCLES,100);
   y=millis();
-  Serial.println(y-x);
+  Serial.printf("Total time: %ld ms\n",y-x);
 
-  c[0]=Pixel::RGB(0,0,0);
-  c[1]=c[0];
-  c[2]=c[1];
-  c[3]=Pixel::RGB(0,0,0);
-  c[4]=c[3];
-  c[5]=Pixel::RGB(0,0,0);
-  c[6]=c[5];
-  c[7]=c[6];
+  Serial.printf("Turning off RGB LEDs.\n");
 
-  px.set(c,8);
+  px.set(Pixel::RGB(0,0,0),8);
 
   Serial.print("Done!\n");  
 }
 
 void loop(){
-  
 }
-
-
-/////////
-
-//#include "RFControl.h"
-//
-//long int x,y;
-//
-//void setup(){     
-// 
-//  Serial.begin(115200);
-//  delay(1000);
-//
-//  Serial.print("\n\nHomeSpan RF Transmitter Example\n\n");
-//
-//  RFControl rf(13,false);         // do not use REF TICK
-//  RFControl rf1(21);              // use REF TICK
-//
-//  Serial.printf("%d %d\n",SOC_RMT_MEM_WORDS_PER_CHANNEL,SOC_RMT_TX_CANDIDATES_PER_GROUP);
-//
-//  rf.clear();                     // clear the pulse train memory buffer
-//  rf1.clear();
-//
-//  #define COUNT   5
-//  #define ONTIME  5000
-//  #define OFFTIME 5000  
-//
-//  for(int i=0;i<COUNT;i++)
-//    rf.add(ONTIME*80,OFFTIME*80);
-//  rf.phase(OFFTIME*80,LOW);
-//
-//  for(int i=0;i<COUNT;i++)
-//    rf1.add(ONTIME,OFFTIME);
-//  rf1.phase(OFFTIME,LOW);  
-//
-//  rf1.enableCarrier(10,0.4);
-//
-//  Serial.print("Starting cycles of pulses...\n");
-//  x=millis();
-//  rf.start(4,100);                // start transmission of 4 cycles of the pulse train with 1 tick=100/80 microseconds
-//  y=millis();
-//  Serial.println(y-x);
-//
-//  Serial.print("Starting cycles of pulses...\n");
-//  x=millis();
-//  rf1.start(4,100);                // start transmission of 4 cycles of the pulse train with 1 tick=100 microseconds
-//  y=millis();
-//  Serial.println(y-x);
-//
-//  Serial.print("Done!\n");
-//  
-//}
-//
-//void loop(){
-//
-//}
-
-///////////////////
