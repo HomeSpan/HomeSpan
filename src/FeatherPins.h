e3a750c76cdf5ea72b9fc774c36cefcd772b9b33
Gregg <homespan@icloud.com>
2024-10-12 08:37:53 -0500

    Update FeatherPins.h

--- src/FeatherPins.h
+++ src/FeatherPins.h
@@ -32,7 +32,8 @@
  
 #if defined(ARDUINO_FEATHER_ESP32)
   enum {
-    F13=13,F12=12,F27=27,F15=15,F32=32,F14=14,F16=16,F17=17,F21=21,   // Digital Only (9 pins)
+    F13=13,F12=12,F27=27,                                             // Digital w/Touch (3 pins)
+    F15=15,F32=32,F14=14,F16=16,F17=17,F21=21,                        // Digital Only (6 pins)
     F26=26,F25=25,F34=34,F39=39,F36=36,F4=4,                          // A0-A5
     F22=22,F23=23,                                                    // I2C SCL/SDA
     F5=5,F18=18,F19=19,F33=33                                         // SPI SCK/SDO/SDI/CS
@@ -41,7 +42,8 @@
 
 #elif defined(ARDUINO_ESP32S2_DEV)
   enum {
-    F13=1,F12=3,F27=7,F15=10,F32=42,F14=11,F16=20,F17=21,F21=16,      // Digital Only (9 pins)
+    F13=1,F12=3,F27=7,                                                // Digital w/Touch (3 pins)
+    F15=10,F32=42,F14=11,F16=20,F17=21,F21=16,                        // Digital Only (6 pins)
     F26=17,F25=14,F34=13,F39=12,F36=18,F4=19,                         // A0-A5
     F22=9,F23=8,                                                      // I2C SCL/SDA
     F5=36,F18=35,F19=37,F33=34,                                       // SPI SCK/SDO/SDI/CS
@@ -61,7 +63,8 @@
 
 #elif defined(ARDUINO_ESP32S3_DEV)
   enum {
-    F13=5,F12=6,F27=7,F15=16,F32=17,F14=18,F16=37,F17=36,F21=38,      // Digital Only (9 pins)
+    F13=5,F12=6,F27=7,                                                // Digital w/Touch (3 pins)
+    F15=16,F32=17,F14=18,F16=37,F17=36,F21=38,                        // Digital Only (6 pins)
     F26=1,F25=2,F34=20,F39=19,F36=15,F4=4,                            // A0-A5
     F22=9,F23=8,                                                      // I2C SCL/SDA
     F5=12,F18=11,F19=13,F33=10,                                       // SPI SCK/SDO/SDI/CS
@@ -71,8 +74,8 @@
   
 #elif defined(ARDUINO_ESP32C6_DEV)
   enum {
-    F12=9,F27=6,F15=11,F32=7,F14=10,F16=13,F17=12,F21=15,             // Digital Only (8 pins)
-    F26=0,F25=1,F34=2,F39=3,F36=4,F4=5,                               // A0-A5
+    F12=9,F27=6,F15=7,F32=10,F14=11,F16=13,F17=12,F21=15,             // Digital Only (8 pins)
+    F26=3,F25=2,F34=1,F39=0,F36=5,F4=4,                               // A0-A5
     F22=22,F23=23,                                                    // I2C SCL/SDA
     F5=21,F18=19,F19=20,F33=18,                                       // SPI SCK/SDO/SDI/CS
     BUILTIN_PIXEL=8                                                   // Built-in Neo-Pixel        
