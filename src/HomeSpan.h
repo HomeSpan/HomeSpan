a397001fa26b27758193d4c1cefd20c48b4c5370
Gregg <homespan@icloud.com>
2024-12-07 21:31:34 -0600

    Update HomeSpan.h

--- src/HomeSpan.h
+++ src/HomeSpan.h
@@ -111,8 +111,8 @@ enum HS_STATUS {
   HS_AP_STARTED,                          // HomeSpan has started the Access Point but no one has yet connected
   HS_AP_CONNECTED,                        // The Access Point is started and a user device has been connected
   HS_AP_TERMINATED,                       // HomeSpan has terminated the Access Point 
-  HS_OTA_STARTED,                         // HomeSpan is in the process of recveived an Over-the-Air software update
-  HS_WIFI_SCANNING                        // HomeSpan is in process of scanning for WiFi networks
+  HS_OTA_STARTED,                         // HomeSpan is in the process of receiving an Over-the-Air software update
+  HS_WIFI_SCANNING                        // HomeSpan is in the process of scanning for WiFi networks
 };
 
 //////////////////////////////////////////////////////////
