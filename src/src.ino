f7ee8c73d494672d812dc431a8cc69911ce186bf
Gregg <homespan@icloud.com>
2025-08-31 21:12:37 -0500

    Squashed commit of the following:
    commit 711b35ff3b2c8c3eeee6a8d4c191d34683b74532
    Author: Gregg <homespan@icloud.com>
    Date:   Sun Aug 31 21:11:10 2025 -0500
    
        Added homeSpan.setWebLogFavicon(const char *)
    
        Allows user to specify href link to hosted favicon in PNG format.  Defaults to HomeSpanLogo.png if argument is left unspecified.
    
    commit e75df4c2d18f22c05b34ddadc8a92b7ac106f979
    Author: Gregg <homespan@icloud.com>
    Date:   Sun Aug 31 15:13:06 2025 -0500
    
        Added centered logo with white background
    
    commit ef7d4d237f41b10c40eeeec363e7fa7ddab06b19
    Author: Gregg <homespan@icloud.com>
    Date:   Sun Aug 31 15:02:01 2025 -0500
    
        Delete HomeSpanLogo_transparent.png
    
    commit aa4e02ba58f826cbab1413661e06d40e1082223b
    Author: Gregg <homespan@icloud.com>
    Date:   Sun Aug 31 15:01:27 2025 -0500
    
        update logos
    
    commit b56a67c71d9c7a77b866f939d3c24b9ac978129e
    Author: Gregg <homespan@icloud.com>
    Date:   Sun Aug 31 14:42:00 2025 -0500
    
        testing favicon image hosted on github

--- src/src.ino
+++ src/src.ino
@@ -26,46 +26,17 @@
  ********************************************************************************/
 
 #include "HomeSpan.h"
-#include "SpanRollback.h"
-
-int watchDogSeconds=0;
 
 void setup() {
  
   Serial.begin(115200);
 
-  delay(1000);
-
-//  homeSpan.setStatusPixel(18);
-  homeSpan.setControlPin(0);
-
-  WiFi.enableIPv6();
-  
-//  homeSpan.enableWatchdog();
   homeSpan.setLogLevel(2);
   homeSpan.enableOTA();
-  homeSpan.setSketchVersion("1.9");
-  homeSpan.enableWebLog(50);
+  homeSpan.enableWebLog(50).setWebLogFavicon("https://raw.githubusercontent.com/HomeSpan/HomeSpan/refs/heads/favicon/docs/images/HomeSpanLogoX.png");
   homeSpan.setCompileTime();
-  homeSpan.setStatusCallback([](HS_STATUS status){Serial.printf("\n*** HOMESPAN STATUS: %s\n\n",homeSpan.statusString(status));});
-
-  new SpanUserCommand('T'," - time delay",[](const char *buf){delay(20000);});
-  new SpanUserCommand('B'," - rollback",[](const char *buf){esp_ota_mark_app_invalid_rollback_and_reboot();});
-  new SpanUserCommand('v'," - validate sketch",[](const char *buf){homeSpan.markSketchOK();});
-  
-  new SpanUserCommand('w'," - watchdog",[](const char *buf){
-    for(int i=0;i<CONFIG_FREERTOS_NUMBER_OF_CORES;i++){
-    TaskHandle_t th;
-    th=xTaskGetIdleTaskHandleForCore(i);
-    char *name=pcTaskGetName(th);
-    boolean wdt=(ESP_OK==esp_task_wdt_status(th));
-    Serial.printf("%s: %s\n",name,wdt?"Enabled":"Disabled");
-  }});
-  
-  new SpanUserCommand('e'," - enable HomeSpan watchdog",[](const char *buf){homeSpan.enableWatchdog(atoi(buf+1));});
-  new SpanUserCommand('d'," - disable HomeSpan watchdog",[](const char *buf){homeSpan.disableWatchdog();});
              
-  homeSpan.begin(Category::Lighting,"HomeSpan Test");
+  homeSpan.begin(Category::Lighting,"HomeSpan Basic");
 
   new SpanAccessory();
     new Service::AccessoryInformation();  
@@ -73,20 +44,5 @@ void setup() {
     new Service::LightBulb();
       new Characteristic::On();
 
-//  homeSpan.setPollingCallback([](){homeSpan.markSketchOK();});
-//  sprintf(NULL,"HERE IS AN ERROR!");
-
-homeSpan.autoPoll(8192,1,1);
-
-//delay(20000);
-//int i=0; while(1) i++;
-
-}
-
-
-//////////////////////////////////////
-
-void loop(){
-  
-//  homeSpan.poll();
+  homeSpan.autoPoll();
 }
