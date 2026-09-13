b8697b248e5814690ed658fc20660913870aba74
Gregg <homespan@icloud.com>
2026-09-12 22:40:01 -0500

    New RemoteSensor Examples based on SpanCast
    Replaces prior examples that used SpanPoint.

---
+++ examples/Other Examples/RemoteSensors/HomeSpanDevice/HomeSpanDevice.ino
@@ -0,0 +1,149 @@
+/*********************************************************************************
+ *  MIT License
+ *  
+ *  Copyright (c) 2020-2026 Gregg E. Berman
+ *  
+ *  https://github.com/HomeSpan/HomeSpan
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *  
+ ********************************************************************************/
+ 
+////////////////////////////////////////////////////////////
+//                                                        //
+//    HomeSpan: A HomeKit implementation for the ESP32    //
+//    ------------------------------------------------    //
+//                                                        //
+//     Demonstrates how to use SpanCast() to implement    //
+//     two remote temperature sensors on standalone       //
+//     ESP32 or ESP8266 devices.                          //
+//                                                        //
+//     This sketch is for the main HomeSpan Device that   //
+//     contains all the usual HomeSpan logic, plus two    //
+//     instances of SpanCast to read and write messages   //
+//     (from and to) the two remote temperature-sensing   //
+//     devices.                                           //
+//                                                        //
+//     Sketches for these remote devices can be found in  //
+//     the IndoorTempSensor and OutdoorTempSensor         //
+//     examples.                                          //
+//                                                        //   
+////////////////////////////////////////////////////////////
+
+#include "HomeSpan.h"             // include HomeSpan as usual
+
+#include "SpanCast.h"             // MUST ALSO INCLUDE THE SPANCAST LIBRARY!
+
+//////////////////////////////////////
+
+struct RemoteTempSensor : Service::TemperatureSensor {
+
+  SpanCharacteristic *temp;
+  SpanCharacteristic *fault;
+  SpanCast *remoteTemp;
+  char *deviceName;
+  uint32_t timer=0;
+  
+  RemoteTempSensor(const char *name, uint8_t deviceID) : Service::TemperatureSensor(){
+
+    asprintf(&deviceName,"%s",name);                         // save name for diagnostic reporting
+    
+    temp=new Characteristic::CurrentTemperature(-10.0);      // create Temperature Characteristic and set initial temperature to -10 Celsius
+    temp->setRange(-50,100);                                 // remember to expand the allowed temperature range to include negative values
+
+    fault=new Characteristic::StatusFault(1);                // create Fault Characteristic and set initial state to true
+
+    // Configure a SpanCast connection to send an arbitrary message of up to 48 bytes to the remote device,
+    // as well as receive a 4-byte message containing the remote device temperature (as a float).  Receipt
+    // of the temperature FROM the remote device is of course the purpose of the sketch.  Sending of an
+    // arbitrary message TO the remote device serves no practical purpose and is only included in this sketch
+    // to show an example of how to both send and receive messages to and from a remote device.
+
+    remoteTemp=new SpanCast(deviceID,48,sizeof(float));      // create a SpanCast with ID=deviceID (passed as a parameter below), send size=48, and receive size=sizeof(float)
+
+  } // end constructor
+
+  void loop(){
+
+    float temperature;
+       
+    if(remoteTemp->get(&temperature)){      // try to read temperature data from the remote sensor
+      temp->setVal(temperature);            // if a message is available, update the temperature characteristic
+      fault->setVal(0);                     // clear fault
+       
+      LOG1("Sensor %s update: Temperature=%0.2f\n",deviceName,temperature*9/5+32);
+      
+    } else if(remoteTemp->time()>60000 && !fault->getVal()){    // else if it has been a while since last update (60 seconds), and there is no current fault
+      fault->setVal(1);                                         // set fault state
+      LOG1("Sensor %s update: FAULT\n",deviceName);
+    }
+
+    if(millis()-timer>10000){               // every 10 seconds, send an arbitrary message (max 48 bytes) back to remote sensor (for illustrative purposes only)
+      timer=millis();
+      char msg[48];
+      sprintf(msg,"Confirming your temp is %0.1f F!",temp->getVal<float>()*9/5+32);
+      LOG1("Sending to %s: %s\n",deviceName,msg);
+      remoteTemp->send(msg);
+    }
+    
+  } // loop
+  
+};
+
+//////////////////////////////////////
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  homeSpan.setLogLevel(1);
+
+  // Configure SpanCast to use Device ID = 18.  Defaults will be used for all other SpanCast settings
+
+  SpanCast::configure(18);
+
+  homeSpan.begin(Category::Bridges,"Sensor Hub");
+
+  new SpanAccessory();  
+    new Service::AccessoryInformation();
+      new Characteristic::Identify(); 
+      
+  new SpanAccessory();
+    new Service::AccessoryInformation();
+      new Characteristic::Identify();
+      new Characteristic::Name("Indoor Temp");
+    new RemoteTempSensor("Indoor Temp",46);               // create remote sensor "Indoor Temp" with SpanCast Device ID = 46
+
+  new SpanAccessory();
+    new Service::AccessoryInformation();
+      new Characteristic::Identify(); 
+      new Characteristic::Name("Outdoor Temp");
+    new RemoteTempSensor("Outdoor Temp",2);               // create remote sensor "Indoor Temp" with SpanCast Device ID = 2
+  
+} // end of setup()
+
+//////////////////////////////////////
+
+void loop(){
+  
+  homeSpan.poll();
+  
+} // end of loop()
+
+//////////////////////////////////////
---
+++ examples/Other Examples/RemoteSensors/TempSensor-Indoor/TempSensor-Indoor.ino
@@ -0,0 +1,77 @@
+/*********************************************************************************
+ *  MIT License
+ *  
+ *  Copyright (c) 2026 Gregg E. Berman
+ *  
+ *  https://github.com/HomeSpan/HomeSpan
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *  
+ ********************************************************************************/
+
+#include "SpanCast.h"  
+  
+///////////////////////////////
+
+SpanCast *mainDevice;     // pointer to SpanCast connection to Main (HomeSpan) Device
+float temp=-10.0;         // variable to store our "simulated" temperature (in degrees C)
+
+#define THIS_DEVICE_NAME    "Indoor Temperature Sensor"
+
+#define THIS_DEVICE_ID      46    // SpanCast Device ID configured for THIS Device
+#define MAIN_DEVICE_ID      18    // SpanCast Device ID of the MAIN Device to which this device sends (and optionally receives) messages
+
+//////////////////////
+
+void setup() {
+
+  Serial.begin(115200);
+  delay(1000); 
+
+  Serial.printf("\n\n%s Ready.\n\n",THIS_DEVICE_NAME);
+
+  SpanCast::configure(THIS_DEVICE_ID,{.channelMask=SpanCast::CHANS_1_11});
+  SpanCast::configure(THIS_DEVICE_ID,{.channelMask=0xFFFF});
+    
+  mainDevice=new SpanCast(MAIN_DEVICE_ID,sizeof(float),48);
+}
+
+//////////////////////
+
+uint32_t aTime=0;
+uint8_t msgData[48];
+
+void loop() {
+
+  if(millis()-aTime>5000){
+
+    Serial.printf("Sending Temperature: %f\n",temp);
+
+    mainDevice->send(&temp);
+
+    temp+=0.5;       // increment the "temperature" by 0.5 C
+    if(temp>35.0)
+      temp=-10.0;
+
+    aTime=millis();
+  }
+
+  if(mainDevice->get(msgData))
+    Serial.printf("Message Received = '%s'\n",msgData);
+}
